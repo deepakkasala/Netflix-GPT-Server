8ae39b231240e6818f58f9e1c27cf859760d8fb3
Deepak Kasala <deepakkasala14@gmail.com>
2025-09-16 17:36:19 +0530

    added logs for debugging prod errors

--- controllers/ai.js
+++ controllers/ai.js
@@ -3,12 +3,15 @@ const main = require("../config/gemini");
 const generateContent = async (req, res) => {
   try {
     const { prompt } = req.body;
+    console.log("prompt Inside ai.js controller", prompt);
 
     const content = await main(
       "Act as a movie recommendation system and suggest some movies for the query: " +
         prompt +
         ". Only give me names of 5 movies, comma separated like the example result given ahead. The result should be in a proper json format lets say example: {result:'Bahubali, RRR, The Conjuring: Last Rites, The Naked Gun, Interstellar'}"
     );
+    console.log("Content inside ai.js controller: ", content);
+
     res.json({ success: true, content });
   } catch (error) {
     res.json({ success: false, error: error.message });

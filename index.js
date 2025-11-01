15d4c07fb477dfffd3e6941997b3b33907e4dc2f
Deepak Kasala <deepakkasala14@gmail.com>
2025-11-01 08:32:55 +0530

    Fix CORS allowed origin URL by removing trailing slash

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://movies-flix-ott-gpt.vercel.app/",
+  "https://movies-flix-ott-gpt.vercel.app",
 ];
 app.use(
   cors({
